4511c15e6784f2b885158dc389611070491a5fe2
Carlos Daniel Faita <carlosfaita99@gmail.com>
2020-03-14 23:09:20 -0300

    Erro Undefined
    Erro de undenfined ao usar o método toLocaleDateString() e toLocaleTimeString()

---
+++ scripts/controller/CalcController.js
@@ -0,0 +1,72 @@
+class CalcController {
+
+  constructor() {
+    this._locale = 'pt-BR'
+    this._displayCalcEl = document.querySelector("#display")
+    this._dateEl = document.querySelector("#data")
+    this._timeEl = document.querySelector("#hora")
+    this._currentDate
+    this.initialize()
+
+  }
+
+  initialize() {
+
+    setInterval(() => {
+
+      this.displayDate = this.currentDate//.toLocaleDateString(this._locale)
+      this.displayTime = this.currentDate//.toLocaleTimeString(this._locale)
+
+    }, 1000)
+
+  }
+
+  get displayTime() {
+
+    return this._timeEl.innerHTML
+
+  }
+
+  set displayTime(value) {
+
+    this._timeEl.innerHTML = value
+
+  }
+
+  get displayDate() {
+
+    return this._dateEl.innerHTML
+
+  }
+
+  set displayDate(value) {
+
+    this._dateEl.innerHTML = value
+
+  }
+
+  get displayCalc(){
+
+    return this._displayCalcEl.innerHTML
+
+  }
+
+  set displayCalc(value) {
+
+    this._displayCalcEl.innerHTML = value
+
+  }
+
+  get dataAtual() {
+
+    return new Date()
+
+  }
+
+  set dataAtual(value) {
+
+    this._currentDate = value
+
+  }
+
+}
