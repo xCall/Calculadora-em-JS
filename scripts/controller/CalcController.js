37b24251fbee170a6dabf927d279c2500a902f1f
Carlos Daniel Faita <carlosfaita99@gmail.com>
2020-03-18 21:26:13 -0300

    Push de codigo produzido ate 17/03/2020
    push de código ate agora já produzido, sem nenhum bug e funcionando do jeito que deveria

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -38,26 +38,39 @@ class CalcController {
   isOperator(value) {
     return (['+', '-', '*', '%', '/'].indexOf(value) > -1)
   }
+  pushOperation(value) {
+    this._operation.push(value)
+    if(this._operation.length > 3) {
+      this.calc()
+    }
+  }
+  calc() {
+    let last = this._operation.pop()
+    let result = eval(this._operation.join(""))
+    this._operation = [result, last]
+  }
+  setLastNumberToDisplay() {
+    
+  }
   addOperation(value) {
-    console.log("a", value, isNaN(this.getLastOperation()))
     if(isNaN(this.getLastOperation())) {
       if(this.isOperator(value)) {
         this.setLastOperation(value) 
       } else if(isNaN(value)) {
-        console.log(value)
+        console.log('esse valor',value)
       } else {
-        this._operation.push(value)
+        this.pushOperation(value)
       }
     } else {
       if(this.isOperator(value)) {
-        this._operation.push(value)
+        this.pushOperation(value)
       } else {
         let newValue = this.getLastOperation().toString() + value.toString()
         this.setLastOperation(parseInt(newValue))
+        this.setLastNumberToDisplay()
       }
     }
     //this._operation.push(value)
-    console.log(this._operation)
   }
   execBtn(value) {
     switch(value) {
